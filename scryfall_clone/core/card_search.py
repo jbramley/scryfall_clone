e1a3c24a2bfd222258226ffab918964c166c4f2c
Justin Bramley <jbramley@gmail.com>
2022-10-09 17:17:02 -0400

    Clean some stuff up

--- scryfall_clone/core/card_search.py
+++ scryfall_clone/core/card_search.py
@@ -7,24 +7,28 @@ from sqlalchemy.orm import Session
 from scryfall_clone import models
 
 
-def _add_column_query_term(stmt, col, query):
+class InvalidQueryTerm(Exception):
+    pass
+
+
+def _add_column_query_term(col, query):
     match col:
         case "o" | "oracle":
-            return stmt.where(models.Card.text.contains(query))
+            return models.Card.text.contains(query)
         case "t" | "type":
-            return stmt.where(sqlalchemy.func.lower(models.Card.types) == query.lower())
+            return sqlalchemy.func.lower(models.Card.types) == query.lower()
         case _:
-            return stmt
+            raise InvalidQueryTerm(query)
 
 
-def _add_query_term(stmt, query):
+def _add_query_term(query):
     match query.split(":"):
         case [q]:
-            return stmt.where(models.Card.name.contains(q))
+            return models.Card.name.contains(q)
         case [k, q]:
-            return _add_column_query_term(stmt, k, q)
+            return _add_column_query_term(k, q)
         case _:
-            return stmt
+            raise InvalidQueryTerm(query)
 
 
 class CardSearch:
@@ -34,7 +38,12 @@ class CardSearch:
     async def search_on_query(self, query: str) -> list[models.Card]:
         query_terms = shlex.split(query)
         stmt = select(models.Card)
+        errors = []
+        wheres = []
         for q in query_terms:
-            stmt = _add_query_term(stmt, q)
-        db_q = await self._session.execute(stmt)
+            try:
+                wheres.append(_add_query_term(q))
+            except InvalidQueryTerm as e:
+                errors.append(e.args)
+        db_q = await self._session.execute(stmt.where(*wheres))
         return db_q.scalars().all()
